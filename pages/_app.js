87489948e6879e2582af032f8cd083588d7d70df
calgui1 <77653871+calgui1@users.noreply.github.com>
2023-02-11 03:14:17 -0600

    Changed theme to Theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from '../components/layouts/main'
-import theme from "../lib/theme";
+import Theme from "../lib/theme";
 
 
 const Website = ({ Component, pageProps, router }) => {
 	return (
-		<ChakraProvider theme={theme}>
+		<ChakraProvider theme={Theme}>
 			<Layout router={router}>
 				<Component {...pageProps} key={router.route} />
 			</Layout>
