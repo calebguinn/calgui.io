1f6a873bbd2598aba5f6a2b29add49582ee3fd81
calgui1 <77653871+calgui1@users.noreply.github.com>
2023-03-05 23:14:43 -0600

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from '../components/layouts/main'
 import Theme from "../lib/theme";
 import Fonts from '../components/fonts';
+import { Analytics } from '@vercel/analytics/react'
 
 
 const Website = ({ Component, pageProps, router }) => {
@@ -10,6 +11,7 @@ const Website = ({ Component, pageProps, router }) => {
       <Fonts />
 			<Layout router={router}>
 				<Component {...pageProps} key={router.route} />
+        <Analytics />
 			</Layout>
 		</ChakraProvider>
 	)
